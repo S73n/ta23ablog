d0d8354494d165b3793a32cb50ca800e24a98c97
S73n <sten.armin@gmail.com>
2025-11-18 10:13:32 +0200

    Add pagination

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PublicController extends Controller
 {
     public function index(){
-            $posts = Post::all();
-            return view('welcome', compact('posts'));
+
+        $posts = Post::paginate(16);
+        return view('welcome', compact('posts'));
+    }
+
+    public function post(Post $post) {
+        return view('post', compact('post'));
     }
 }
